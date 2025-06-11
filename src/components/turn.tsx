7de8c94bb76c6d530d01641a16e5c4f17b89ac4f
akita0724 <118238952+akita0724@users.noreply.github.com>
2025-06-11 14:10:40 +0900

    turn.tsx を更新

--- src/components/turn.tsx
+++ src/components/turn.tsx
@@ -1,7 +1,7 @@
 export const Right_Turn = ({ turn }: { turn: number }) => {
   return (
     <div
-      className="flex flex-col items-center justify-centerbg-gray-100 absolute right-0 top-0 h-full w-1/12 bg-blue-300"
+      className="flex flex-col items-center justify-center absolute right-0 top-0 h-full w-1/12 bg-blue-300"
       hidden={turn !== 1}
     />
   );
@@ -10,7 +10,7 @@ export const Right_Turn = ({ turn }: { turn: number }) => {
 export const Left_Turn = ({ turn }: { turn: number }) => {
   return (
     <div
-      className="flex flex-col items-center justify-centerbg-gray-100 absolute left-0 top-0 h-full w-1/12 bg-red-300"
+      className="flex flex-col items-center justify-center absolute left-0 top-0 h-full w-1/12 bg-red-300"
       hidden={turn !== -1}
     />
   );
